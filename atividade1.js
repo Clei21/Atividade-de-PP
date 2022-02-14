ec908c1ed99f6585dfd9d56bc7d67b7bb190ace4
Clei21 <cma.lic20@uea.edu.br>
2022-02-14 20:11:15 -0300

    Verifica os números pares e impares

--- atividade1.js
+++ atividade1.js
@@ -1,11 +1,31 @@
 //Detectar se é um número par ou impar
-exibirTipo(10);
-function exibirTipo(Detectar) {
-    for (var i = 0; i <= Detectar; i++) {
-        if (i % 2 === 0)
-        console.log (i,'par');
-        else
-        console.log (i,'impar');
-    
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout 
+});
+
+function showTypeOfNumber(number){
+    if(number %2 == 0){
+        console.log(number + " é um número par")
+    } else {
+        console.log(number + " é um número ímpar")
     }
 }
+
+rl.question('Digite um número válido ou digite X para sair: ', function (number) {
+    if(number === 'x' || number === 'X'){
+        rl.close();
+    }
+
+    showTypeOfNumber(number);
+
+    rl.close();
+});
+
+rl.on('close', function () {
+    console.log('\nAté mais !!!');
+    process.exit(0);
+});
